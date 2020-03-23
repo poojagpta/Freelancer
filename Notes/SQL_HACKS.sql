d6dc8ef5f052dba7305dc6f204fea62ca1d9c1ab
poojagpta <pooja.fiserv@gmail.com>
2020-03-23 10:53:34 -0700

    Update SQL_HACKS.sql

--- Notes/SQL_HACKS.sql
+++ Notes/SQL_HACKS.sql
@@ -1,3 +1,9 @@
 1. Find the first value order by device_events_ts, we can use below hack:
 
     MAX(STRUCT(device_event_ts, build)).col2 AS build
+               
+2. Concat all non-null string
+       concat_ws('&', collect_set(IF(concat(test_id,':',bucket)  LIKE '%:%', concat(test_id,':',bucket) , NULL)))
+                                     as active_exp_map
+                                     
+3. Other things                                    
