b2e4c6a3d27e3052b57bfcf0c48408bdd75d6066
poojagpta <pooja.fiserv@gmail.com>
2020-05-01 10:18:09 -0700

    Update SQL_HACKS.sql

--- Notes/SQL_HACKS.sql
+++ Notes/SQL_HACKS.sql
@@ -6,4 +6,13 @@
        concat_ws('&', collect_set(IF(concat(test_id,':',bucket)  LIKE '%:%', concat(test_id,':',bucket) , NULL)))
                                      as active_exp_map
                                      
-3. Other things                                    
+3. Check if file has schema to apply on:
+
+SELECT * FROM
+(select get_json_object(regexp_replace(devices, '\\[|\\]', ''), '$.esn') AS dd, *  from
+sbschema.roku_fact_web_logs12 
+where  date_key='2020-04-26' AND action not in ('Pageview')) t
+WHERE dd is NULL
+AND COALESCE(devices,'') NOT IN ('','[{}]')
+
+4. 
